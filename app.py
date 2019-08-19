7515d501ae2b4694b3bd5867a50059d3c47b5c2c
anemone3504 <anemone3504@gmail.com>
2019-08-19 23:54:19 +0900

    ちょっとした変更

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback",methods=['POST'])
 def callback():
-    signature = request.handlers['X-Line-Signature']
-    body = request.get_data(app_text = True)
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text = True)
     app.logger.info("Request body: " + body)
 
     try:
