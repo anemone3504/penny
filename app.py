9fea240e0f40f30eddc82a1485ebd111ebe94494
Aman0jaku <nagim0rin0bu3000@i.softbank.jp>
2019-08-21 01:03:31 +0900

    不具合修正

--- app.py
+++ app.py
@@ -118,20 +118,7 @@ def handle_postback(event):
         message = FlexSendMessage(alt_text = "1週間の貯金額", contents = bubble)
         line_bot_api.reply_message(
             event.reply_token,
-            message,
-            quick_reply = QuickReply(
-                items = [
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1週間分の貯金額",data = "1週間")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1ヶ月分の貯金額",data = "1ヶ月")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1年分の貯金額",data = "1年分")
-                    )
-                ]
-            )
+            message
         )
 
     elif event.postback.data == '1ヶ月':
@@ -183,23 +170,10 @@ def handle_postback(event):
         message = FlexSendMessage(alt_text = "1ヶ月の貯金額", contents = bubble)
         line_bot_api.reply_message(
             event.reply_token,
-            message,
-            quick_reply = QuickReply(
-                items = [
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1週間分の貯金額",data = "1週間")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1ヶ月分の貯金額",data = "1ヶ月")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1年分の貯金額",data = "1年分")
-                    )
-                ]
-            )
+            message
         )
 
-    elif event.postback.data == '1年':
+    else:
         #1年前の日付を取得
         purpose_date = x - relativedelta(years = 1)
         purpose_date = purpose_date.isoformat()
@@ -248,20 +222,7 @@ def handle_postback(event):
         message = FlexSendMessage(alt_text = "1年の貯金額", contents = bubble)
         line_bot_api.reply_message(
             event.reply_token,
-            message,
-            quick_reply = QuickReply(
-                items = [
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1週間分の貯金額",data = "1週間")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1ヶ月分の貯金額",data = "1ヶ月")
-                    ),
-                    QuickReplyButton(
-                        action = PostbackAction(label = "1年分の貯金額",data = "1年分")
-                    )
-                ]
-            )
+            message
         )
 
 #テキストメッセージが送られたときのイベント
