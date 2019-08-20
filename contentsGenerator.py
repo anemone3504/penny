998db9a2f2094e66a1e11bc2736654cfcef4f86d
Aman0jaku <nagim0rin0bu3000@i.softbank.jp>
2019-08-20 23:37:00 +0900

    bug fix

--- contentsGenerator.py
+++ contentsGenerator.py
@@ -7,10 +7,11 @@ def gen(exist_dates,results):
     #外側の配列
     BoxComponents = []
     for i in range(exist_dates):
+        strdate=results[i][0].isoformat()#JSONうんたらがdate型はだめいわれてる？みたいだから文字列変換しますからぁ！
         ele=BoxComponent(
             layout = 'baseline',
             contents = [
-                TextComponent(text = results[i][0],size = 'sm',flex = 1,color = '#555555'),
+                TextComponent(text = strdate,size = 'sm',flex = 1,color = '#555555'),
                 TextComponent(text = str(results[i][1]) + '円',size = 'sm',align = 'end',color = '#111111')
             ]
         )
