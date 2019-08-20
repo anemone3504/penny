ccc1294aabd503f99679bde0203d7c00befa09ca
Aman0jaku <nagim0rin0bu3000@i.softbank.jp>
2019-08-21 01:24:46 +0900

    今日最後

--- app.py
+++ app.py
@@ -224,21 +224,24 @@ def handle_postback(event):
             event.reply_token,
             message
         )
+        
     line_bot_api.reply_message(
         event.reply_token,
-        text = '次は何が見たい？',
-        quick_reply = QuickReply(
-            items = [
-                QuickReplyButton(
-                    action = PostbackAction(label = "1週間分の貯金額",data = "1週間")
-                ),
-                QuickReplyButton(
-                    action = PostbackAction(label = "1ヶ月分の貯金額",data = "1ヶ月")
-                ),
-                QuickReplyButton(
-                    action = PostbackAction(label = "1年分の貯金額",data = "1年")
-                )
-            ]
+        TextSendMessage(
+            text = '次は何が見たい？',
+            quick_reply = QuickReply(
+                items = [
+                    QuickReplyButton(
+                        action = PostbackAction(label = "1週間分の貯金額",data = "1週間")
+                    ),
+                    QuickReplyButton(
+                        action = PostbackAction(label = "1ヶ月分の貯金額",data = "1ヶ月")
+                    ),
+                    QuickReplyButton(
+                        action = PostbackAction(label = "1年分の貯金額",data = "1年")
+                    )
+                ]
+            )
         )
     )
 
